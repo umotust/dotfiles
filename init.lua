bb8907aebc0efc1c2067f5f89df82e07c391f184
umotust <oozora78952@yahoo.co.jp>
2025-11-06 22:21:04 +0900

    refactor(nvim): enable lazy loading for plugins and clean up init.lua
    - Introduced lazy-loading (`event`, `keys`, `cmd`) to most plugins for faster startup
    - Simplified and reorganized plugin definitions under `lazy.nvim`
    - Moved individual plugin configs (cmp, treesitter, overseer, gitsigns, etc.) into lazy specs
    - Removed redundant runtime configurations after plugin setup
    - Improved local config handling (`init.local.lua`) with safer table merging
    - Minor cleanup of indentation, comments, and keymap consistency

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 ---@diagnostic disable: undefined-global
+
 -- Basic
 vim.opt.backspace = { "indent", "eol", "start" }
+
 -- Appearance
 vim.opt.number = true
 vim.opt.cursorline = true
@@ -9,30 +11,31 @@ vim.opt.title = true
 vim.opt.showcmd = true
 vim.opt.display = "lastline"
 vim.opt.updatetime = 1000
--- Color
 vim.opt.termguicolors = true
--- Tab/Indent
+
+-- Indent / Tab
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.autoindent = true
 vim.opt.smartindent = true
+
 -- Search
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
 -- Others
 vim.opt.virtualedit:append("block")
 vim.opt.belloff = "all"
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
 vim.opt.splitright = true
-vim.opt.ttyfast = true
 vim.opt.clipboard:append("unnamed")
 vim.o.mouse = ""
--- Key map
+
+-- Keymap
 vim.keymap.set("n", "Y", "y$")
 vim.keymap.set("n", "<Left>", "<C-w>h")
 vim.keymap.set("n", "<Down>", "<C-w>j")
@@ -58,43 +61,67 @@ vim.keymap.set('n', '<Leader>q', function()
   if quickrun_buf ~= -1 then
     vim.api.nvim_buf_delete(vim.fn.bufnr('quickrun://output'), {force = true})
   end
-end, {silent = true, noremap = true})
+end, { silent = true, noremap = true })
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
--- Lazy.nvim setup (Plugin manager)
+-- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
 local plugins = {
-  -- UI / Theme
   { "cocopon/iceberg.vim" },
   { "vim-airline/vim-airline" },
 
   -- Movement / Editing
-  { "easymotion/vim-easymotion" },
-  { "ntpeters/vim-better-whitespace" },
-  { "tpope/vim-surround" },
-  { "tpope/vim-commentary" },
+  {
+    "easymotion/vim-easymotion",
+    keys = {
+      { "<leader>e", "<Plug>(easymotion-bd-e)", mode = "n" },
+      { "<leader>w", "<Plug>(easymotion-bd-w)", mode = "n" },
+      { "<leader>f", "<Plug>(easymotion-f)",  mode = "n" },
+      { "<leader>F", "<Plug>(easymotion-F)",  mode = "n" },
+      { "<leader>j", "<Plug>(easymotion-j)",  mode = "n" },
+      { "<leader>k", "<Plug>(easymotion-k)",  mode = "n" },
+    },
+    init = function() vim.g.EasyMotion_smartcase = 1 end,
+  },
+  {
+    "ntpeters/vim-better-whitespace",
+    event = { "BufWritePre", "InsertLeave" },
+    init = function()
+      vim.g.strip_whitespace_on_save = 1
+      vim.g.strip_whitespace_confirm = 0
+    end,
+  },
+  { "tpope/vim-surround", event = "VeryLazy" },
+  { "tpope/vim-commentary", event = "VeryLazy" },
 
   -- Search
-  { "junegunn/fzf" },
-  { "junegunn/fzf.vim" },
+  { "junegunn/fzf", build = ":call fzf#install()" },
+  {
+    "junegunn/fzf.vim",
+    keys = {
+      { ",fb", ":Buffers<CR>", mode = "n"},
+      { ",fc", ":Commands<CR>", mode = "n"},
+      { ",ff", ":Files<CR>",  mode = "n"},
+      { ",fh", ":History<CR>", mode = "n"},
+      { ",fl", ":Lines<CR>",  mode = "n"},
+      { ",ft", ":Tags<CR>",   mode = "n"},
+    }
+  },
 
   -- Syntax / Highlight
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "t9md/vim-quickhl"},
-
   -- LSP + Completion
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
+  { "neovim/nvim-lspconfig", event = { "BufReadPre", "BufNewFile" } },
+  { "hrsh7th/cmp-nvim-lsp", event = "VeryLazy" },
   { "mason-org/mason-lspconfig.nvim",
+    event = "VeryLazy",
     opts = {
       ensure_installed = {
         "bashls",
@@ -111,48 +138,85 @@ local plugins = {
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
+      },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = { { name = "nvim_lsp" }, { name = "buffer" } },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "t9md/vim-quickhl",
+    keys = {
+      { "<space>m", "<Plug>(quickhl-manual-this)", mode = { "n", "x" } },
+      { "<space>M", "<Plug>(quickhl-manual-reset)", mode = { "n", "x" } },
     },
   },
 
   -- Git
-  { "lewis6991/gitsigns.nvim" },
-  { "tpope/vim-fugitive" },
-  { "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPost",
+    config = function()
+      require("gitsigns").setup({ update_debounce = 1000 })
+    end,
+  },
+  { "tpope/vim-fugitive", cmd = "Git" },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-        { "<leader>g", "<cmd>LazyGit<CR>", desc = "LazyGit" }
-    }
+    keys = { { "<leader>g", "<cmd>LazyGit<CR>", desc = "LazyGit" } },
   },
 
-  -- Task runner (QuickRun replacement)
-  { "stevearc/overseer.nvim" },
+  -- Task runner
+  {
+    "stevearc/overseer.nvim",
+    cmd = { "OverseerToggle", "OverseerRun" },
+    keys = {
+      { "<leader>R", "<cmd>OverseerToggle<CR>", desc = "Toggle Overseer" },
+      {
+        "<leader>r",
+        function() require("overseer").run_template({ name = "quickrun_" .. vim.bo.filetype }) end,
+        desc = "QuickRun current file",
+      },
+    },
+    config = function()
+      require("overseer").setup({ templates = { "builtin", "user.quickrun" } })
+    end,
+  },
 }
 
--- Allow user to provide a local file that RETURNS a table of additional plugin specs.
--- The recommended shape for `init.local.lua` is:
---   return {
---     { "author/plugin", config = function() ... end },
---   }
+-- Merge local plugin settings if available
 local local_init = vim.fn.stdpath("config") .. "/init.local.lua"
 if vim.fn.filereadable(local_init) == 1 then
   local ok, res = pcall(dofile, local_init)
-  if not ok then
+  if ok and type(res) == "table" then
+    vim.list_extend(plugins, res)
+  elseif not ok then
     vim.notify("Failed to load init.local.lua: " .. res, vim.log.levels.WARN)
-  else
-    -- If the local file returns a table, merge it into the main plugins list.
-    -- If it returns nil or another type, that's fine — dofile already executed
-    -- the file so any side-effect (keymaps, settings) will have run.
-    if type(res) == "table" then
-      for _, p in ipairs(res) do table.insert(plugins, p) end
-    end
   end
 end
 
@@ -160,13 +224,14 @@ require("lazy").setup(plugins)
 
 -- ------------------------------------------------------------
 -- Plugin Configurations
-vim.cmd("colorscheme iceberg")
+vim.cmd.colorscheme("iceberg")
 
 -- Lsp
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "LSP: [G]oto [D]efinition" })
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "LSP: [G]oto [D]eclaration" })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "LSP: [G]oto [R]eference" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
 
+-- Diagnostic float
 vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
     local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
@@ -189,101 +254,22 @@ vim.api.nvim_create_autocmd("CursorHold", {
   end,
 })
 
--- overseer
-require("overseer").setup({
-  templates = { "builtin", "user.quickrun" }
-})
-vim.keymap.set("n", "<leader>R", ":OverseerToggle<CR>")
-vim.keymap.set("n", "<leader>r", function()
-  require("overseer").run_template({ name = "quickrun_" .. vim.bo.filetype })
-end, { desc = "QuickRun current file" })
-
--- Treesitter: syntax highlighting
-require("nvim-treesitter.configs").setup({
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
--- Gitsigns: git diff indicators
-require("gitsigns").setup({
-  update_debounce = 1000,
-})
-
--- LSP and completion setup
-local cmp = require("cmp")
-cmp.setup({
-  mapping = {
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-  },
-})
-
--- Search
-vim.keymap.set("n", ",fb", ":<C-u>Buffers<CR>")
-vim.keymap.set("n", ",fc", ":<C-u>Commands<CR>")
-vim.keymap.set("n", ",ff", ":<C-u>Files<CR>")
-vim.keymap.set("n", ",fh", ":<C-u>History<CR>")
-vim.keymap.set("n", ",fl", ":<C-u>Lines<CR>")
-vim.keymap.set("n", ",ft", ":<C-u>Tags<CR>")
-
-local function get_search_word()
-  local query = ""
-  if query == "" then
-    query = vim.fn.expand("<cword>")
-  end
-  return query
-end
-
+-- Search using rg + fzf
 if vim.fn.executable("rg") == 1 then
   vim.keymap.set("n", ",fg", ":Rg<CR>", { silent = true })
-
-  vim.api.nvim_create_user_command("SearchWord", function(opts)
-    local query = get_search_word()
+  vim.api.nvim_create_user_command("SearchWord", function()
+    local q = vim.fn.expand("<cword>")
     vim.fn["fzf#vim#grep"](
       "rg --column --line-number --no-heading --color=always --smart-case -- .",
-      vim.fn["fzf#vim#with_preview"]({ options = "--query=" .. query }),
-      opts.bang and 1 or 0
+      vim.fn["fzf#vim#with_preview"]({ options = "--query=" .. q }),
+      0
     )
-  end, { bang = true, nargs = "?" })
-
+  end, {})
   vim.keymap.set("n", ",fG", ":SearchWord<CR>", { silent = true, desc = "Search current word (rg)" })
 end
 
--- Easymotion settings
-vim.g.EasyMotion_smartcase = 1
-vim.keymap.set("n", "<leader>e", "<Plug>(easymotion-bd-e)", { remap = true })
-vim.keymap.set("n", "<leader>w", "<Plug>(easymotion-bd-w)", { remap = true })
-vim.keymap.set("n", "<leader>f", "<Plug>(easymotion-f)", { remap = true })
-vim.keymap.set("n", "<leader>F", "<Plug>(easymotion-F)", { remap = true })
-vim.keymap.set("n", "<leader>j", "<Plug>(easymotion-j)", { remap = true })
-vim.keymap.set("n", "<leader>k", "<Plug>(easymotion-k)", { remap = true })
-
 -- Terminal
 vim.keymap.set("n", "<Leader>t", function()
-  vim.cmd(":split | term")
+  vim.cmd("split | term")
   vim.cmd("startinsert")
 end, { noremap = true, silent = true })
-
--- quickhl
-vim.keymap.set("n", "<space>m", "<Plug>(quickhl-manual-this)", { remap = true })
-vim.keymap.set("x", "<space>m", "<Plug>(quickhl-manual-this)", { remap = true })
-vim.keymap.set("n", "<space>M", "<Plug>(quickhl-manual-reset)", { remap = true })
-vim.keymap.set("x", "<space>M", "<Plug>(quickhl-manual-reset)", { remap = true })
-
--- Whitespace cleanup
-vim.g.strip_whitespace_on_save = 1
-vim.g.strip_whitespace_confirm = 0
-
--- Local configurations
-local local_init = vim.fn.stdpath("config") .. "/init.local.lua"
-if vim.fn.filereadable(local_init) == 1 then
-    local ok, err = pcall(dofile, local_init)
-    if not ok then
-        vim.notify("Failed to load init.local.lua: " .. err, vim.log.levels.WARN)
-    end
-end
